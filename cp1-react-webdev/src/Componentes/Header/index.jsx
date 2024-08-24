9f0a72051babba24471d4c2acfa38831f2bb0fce
wendelldsant <wendell_dsantos@hotmail.com>
2024-08-24 17:54:27 -0300

    Add-Header Create

---
+++ cp1-react-webdev/src/Componentes/Header/index.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return ( 
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+     );
+}
+
+export default Header;  
