df996cfdcb651176ac5c1a087ed40b9e6f479e36
wendelldsant <wendell_dsantos@hotmail.com>
2024-08-26 22:37:07 -0300

    Fix-Sistema de Rotas

--- cp1-react-webdev/src/App.jsx
+++ cp1-react-webdev/src/App.jsx
@@ -1,48 +1,18 @@
 import React from 'react';
 import Header from './Componentes/Header';
-import MainContainHome from './Componentes/MainContainHome';
-import SobreNos from './Componentes/SobreNos';
-import Contato from './Componentes/Contato';
-import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {Outlet} from 'react-router-dom'
 
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: (
-        <>
-          <Header />
-          <MainContainHome />
-        </>
-      ) 
-    },
-    {
-      path: "/sobre-nos",
-      element: (
-        <>
-          <Header />
-          <SobreNos />
-        </>
-      ) 
-    },
-    {
-      path: "/contato",
-      element: (
-        <>
-          <Header />
-          <Contato />
-        </>
-      ) 
-    },
-
-  ])
-
 
   return (
-      <RouterProvider router={router}></RouterProvider>
+      <>
+        <Header/>
+         <Outlet/>
+      </>
   )
 }
 
 export default App
+
+
--- cp1-react-webdev/src/main.jsx
+++ cp1-react-webdev/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import MainContainHome from './Componentes/MainContainHome.jsx'
+import SobreNos from "./Componentes/SobreNos.jsx";
+import Contato from "./Componentes/Contato.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {index:true, element: <MainContainHome/>},
+      {path:'sobre', element: <SobreNos/>},
+      {path:'contato', element: <Contato/>}
+    ]
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
 )
